c91463aab940b80a194da4084035288b3c7c00c4
Deeksha <icode.dev4@gmail.com>
2016-01-23 16:01:01 +0530

    commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ShopifyMetafields::Application.config.secret_key_base = 'bc955efbb16ad40d24fd5d3fedc3d2d8-1453459685'
+ShopifyMetafields::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
